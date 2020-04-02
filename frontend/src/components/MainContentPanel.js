0fedf8b8e919eb5cc9bd044199612b226f39e5cd
Andrey Konopkov <konopkov@gmail.com>
2020-04-02 18:47:37 +0300

    fix: useEffect dependency array corrected (#12)

--- frontend/src/components/MainContentPanel.js
+++ frontend/src/components/MainContentPanel.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import Configuration from './Configuration';
+import {getDeviceHostAddress} from '../helpers/utils';
 
+const appConfigUrl = `${getDeviceHostAddress()}/config`;
+const appStatusUrl = `${getDeviceHostAddress()}/status`;
 
 class MainContentPanel extends Component {
     render() {
         return (
-                <Configuration/>
+                <Configuration
+                    appConfigUrl={appConfigUrl}
+                    appStatusUrl={appStatusUrl}
+                />
         );
     }
 }
