620b1c585a841d830e9db8237bbd27dc8df54149
GitHub Action <action@github.com>
2020-04-22 07:58:33 +0000

    Frontend build

---
+++ server/public/precache-manifest.023cc5904f554ef45d539ac783cb0ee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7336bfa0197f60b9584d13ac0e3338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec981a35afaa7d341c8c",
+    "url": "/static/js/2.8cb8a630.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8cb8a630.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad42f99bbb10be8775bb",
+    "url": "/static/js/main.5d410ab4.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
